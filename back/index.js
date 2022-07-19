4db3bddbe6546e6fac222cd75193535fb4c28edf
pache0015 <pache.facundo@gmail.com>
2022-07-19 15:07:24 -0300

    En index se agregan endpoints

--- back/index.js
+++ back/index.js
@@ -1,27 +1,107 @@
 const express = require("express")
 const app = express()
 
-let respuesta = {
-    author: {
-        name: 'Facundo',
-        lastname: 'Pacheco'
-    }
-   };
+const { MeliManager } = require("./meliManager");
+
+const author = {
+    name: 'Facundo',
+    lastname: 'Pacheco'
+}
+
 //Endpoint a usar en: Resultado de busqueda
 app.route('/api/items')
 .get((req, res) => {
-    res.send(req.query.q)
+
+    const query = req.query.q
+    const infoItems = new Promise((resolve, reject) =>{
+        const info = new MeliManager().searchItems(query)
+        resolve(info)
+    })
+
+    infoItems
+        .then( info =>{
+            let _items = []
+            info.forEach(i => {
+                const item = {
+                    id: i.id,
+                    title: i.title,
+                    price:{
+                        currency: i.currency_id,
+                        amount: i.price,
+                        decimals: 0
+                    },
+                    picture: i.thumbnail,
+                    condition: i.condition,
+                    free_shipping: i.shipping.free_shipping
+                }
+                _items.push(item)
+            });
+            const id_category = info[0].category_id
+            const categories = new Promise((resolve, reject) =>{
+                const info = new MeliManager().getCategories(id_category)
+                resolve(info)
+            })
+            // Asumí que la categorías a mostrar para hacer el breadcrumb 
+            //  son las del primer elemento que se trae al hacer la búsqueda.
+            categories
+                .then(categories =>{
+                    let _categories = []
+                    categories.forEach( c =>{
+                        _categories.push(c.name)
+                    })
+                    res.json({
+                        author: author,
+                        categories: _categories,
+                        items : _items
+                    })
+                })
+        })
 });
 
 //Endpoint a usar en: Detalle de producto
 app.route('/api/items/:id')
 .get((req, res)=> {
-    const id = req.params.id
-    
-    res.send(id)
+    const _id = req.params.id
+    let _item = {}
+    const infoItem = new Promise((resolve, reject) =>{
+        const item = new MeliManager().getItem(_id)
+        resolve(item)
+    })
+    const description = new Promise((resolve, reject) =>{
+        const desc   = new MeliManager().getItemDescription(_id)
+        resolve(desc)
+    })
+    infoItem
+        .then((item) =>{
+            _item ={
+                id: item.id,
+                title: item.title,
+                price:{
+                    currency: item.currency_id,
+                    amount: item.price,
+                    decimals: 0
+                },
+                picture: item.thumbnail,
+                condition: item.condition,
+                free_shipping: item.shipping.free_shipping,
+                sold_quantity: item.sold_quantity
+            }
+            
+            description
+                .then(desc => {
+                    _item.description = desc.plain_text
+                    res.json({
+                        author: author,
+                        item : _item
+                    })
+                })
+        })
+        .catch( err => {
+            res.json(err)
+        })
 });
 
-app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000")
+app.listen(8080, () => {
+    console.log("El servidor está inicializado en el puerto 8080")
 });
 
