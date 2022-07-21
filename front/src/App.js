2a7b401a3f1cfe135dd4f8ed578eea562bbf8086
pache0015 <pache.facundo@gmail.com>
2022-07-20 21:10:06 -0300

    Se agregan rutas a componentes

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import Content from './components/content/Content';
+import Content from './pages/content/Content';
+import ResultContent from './pages/resultContent/ResultContent';
+import ResultDescription from './pages/resultDescription/ResultDescription';
 import Searchbar from './components/searchbar/Searchbar';
 import { ContextProvider } from "./context/Context";
+import { Route, Switch } from "wouter";
 
 function App() {
   return (
     <div className="App">
         <ContextProvider>
           <Searchbar />
-          <Content />
+          <Switch>
+            <Route component={Content} path="/" />
+            <Route component={ResultContent} path="/items/search=:search" />
+            <Route component={ResultDescription} path="/items/:id" />
+          </Switch>
         </ContextProvider>
     </div>
   );
